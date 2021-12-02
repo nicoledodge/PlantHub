99c5d1b8854c8e13307b414c5a8bbd9a50c24f52
christa-baccas <christa.baccas@gmail.com>
2021-12-02 14:26:40 -0600

    added blog/post creation method to the seed.js file

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -5,35 +5,27 @@ const plantSeeds = require('./plantSeeds.json');
 const blogSeeds = require('./blogSeeds.json');
 
 db.once('open', async () => {
-  await Blog.deleteMany({});
+  try{
+  // await Blog.deleteMany({});
   await Blog.create(blogSeeds);
-
+  await Plant.deleteMany({});
+  await User.deleteMany({});
+  await User.create(userSeeds);
+  for (let i = 0; i < plantSeeds.length; i++) {
+      const { _id } = await Plant.create(plantSeeds[i]);
+      const user = await User.findOneAndUpdate(
+        { email: "bkernighan@techfriends.dev" },
+        {
+          $addToSet: {
+            myPlants: _id,
+          },
+        }
+      );
+    }
+  } catch(err){
+    console.log(err)
+    process.exit(1)
+  }
   console.log('all done!');
   process.exit(0);
 });
-
-// db.once('open', async () => {
-//   try {
-//     await Plant.deleteMany({});
-//     await User.deleteMany({});
-//     await User.create(userSeeds);
-    
-//     for (let i = 0; i < plantSeeds.length; i++) {
-//       const { _id } = await Plant.create(plantSeeds[i]);
-//       const user = await User.findOneAndUpdate(
-//         { email: "bkernighan@techfriends.dev" },
-//         {
-//           $addToSet: {
-//             myPlants: _id,
-//           },
-//         }
-//       );
-//     }
-//   } catch (err) {
-//     console.error(err);
-//     process.exit(1);
-//   }
-
-//   console.log('all done!');
-//   process.exit(0);
-// });
