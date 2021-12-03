1157a0f1310bedb8001e7df1b55c5e83992cfcb3
Ismeny Saguilan <menysag2@gmail.com>
2021-12-03 12:07:19 -0600

    added about us section

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -4,6 +4,10 @@ export default function About() {
     return (
         <>
         <h2>ABOUT PAGE</h2>
+        <p>Welcome to PlantHub, your number one tool to help keep your plants alive and thriving. We're dedicated to providing you the best platform to track your plants needs. We hope you enjoy our services as much as we enjoy offering them to you.
+        <br></br>
+            Sincerely,<br></br>
+            PlantHub</p>
         </>
     )
 }
