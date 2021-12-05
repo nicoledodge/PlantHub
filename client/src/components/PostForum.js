967ce80aaa254c4ff257567a597db0585e89f673
christa-baccas <christa.baccas@gmail.com>
2021-12-04 22:36:58 -0600

    added comments to be populated

--- client/src/components/PostForum.js
+++ client/src/components/PostForum.js
@@ -19,18 +19,34 @@ function GetPosts() {
   return (
     <div class="ui minimal comments">
       <h3 class="ui dividing header">Comments</h3>
-      {post.map((val) => (
-        <div class="comment" key={`post = ${val._id}`}>
+      {post.map((post) => (
+        <div class="comment" key={`post = ${post._id}`}>
           <div class="content post">
-            <a class="author">{val.postCreator}</a>
+            <a class="author">{post.postCreator}</a>
             <div class="metadata">
-              <span class="date">{val.createdAt}</span>
+              <span class="date">{post.createdAt}</span>
             </div>
-            <div class="text">{val.postText}</div>
+            <div class="text">{post.postText}</div>
             <div class="actions">
               <a class="reply">Reply</a>
             </div>
           </div>
+          {post.comments.map((comment) => (
+            <div class="comments comment">
+              <div class="comment">
+                <div class="content">
+                  <a class="author">{comment.commentCreator}</a>
+                  <div class="metadata">
+                    <span class="date">{comment.createdAt}</span>
+                  </div>
+                  <div class="text">{comment.commentText}</div>
+                  <div class="actions">
+                    <a class="reply">Reply</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       ))}
     </div>
