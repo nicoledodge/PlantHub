4056fe631f346c42e7272c4c692380528b4c84ba
Ismeny Saguilan <menysag2@gmail.com>
2021-12-06 15:58:20 -0600

    testing modals

--- client/src/pages/Team.js
+++ client/src/pages/Team.js
@@ -60,7 +60,9 @@ export default function Team() {
           Get in touch with me!
           </Typography>
           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            Github:
+            Email:
+            Portfolio:
           </Typography>
         </Box>
       </Modal>
@@ -93,7 +95,7 @@ export default function Team() {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
+          <Typography id="modal-modal-title" variant="h6" component="h2" style={{textAlign: 'center'}}>
             Text in a modal
           </Typography>
           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
