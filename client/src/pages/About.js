33df09bd798f3af172ce45b25dd45c6e832039c5
Ismeny Saguilan <menysag2@gmail.com>
2021-12-07 11:45:03 -0600

    added margins to about us page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -5,12 +5,17 @@ import {Segment} from 'semantic-ui-react'
 export default function About() {
     return (
         <>
+     
         <img className="about-img" src={aboutImage} alt={'vines'}/>
+        <div className='about-container'>
         <h2 style={{fontFamily:'Oswald, sans-serif'}}>ABOUT US</h2>
-        <p>Welcome to PlantHub, your number one tool to help keep your plants alive and thriving. We're dedicated to providing you the best platform to track your plants needs. We hope you enjoy our services as much as we enjoy offering them to you.
+        <p
+      className='about-child'>Welcome to PlantHub! Your number one tool to help keep your plants alive and thriving. We're dedicated to providing you the best platform to track your plants needs. We hope you enjoy our services as much as we enjoy offering them to you.
         <br></br>
             Sincerely,<br></br>
             PlantHub</p>
+                
+    </div>
         </>
     )
 }
