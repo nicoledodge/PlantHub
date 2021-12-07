4c2c56b592b924730d60263ef47d0cc2e082deaf
Ismeny Saguilan <menysag2@gmail.com>
2021-12-07 11:08:57 -0600

    added img

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import aboutImage from './assets/vines.jpeg';
+import {Segment} from 'semantic-ui-react'
 
 export default function About() {
     return (
         <>
+        <img className="about-img" src={aboutImage} alt={'vines'}/>
         <h2 style={{fontFamily:'Oswald, sans-serif'}}>ABOUT US</h2>
         <p>Welcome to PlantHub, your number one tool to help keep your plants alive and thriving. We're dedicated to providing you the best platform to track your plants needs. We hope you enjoy our services as much as we enjoy offering them to you.
         <br></br>
