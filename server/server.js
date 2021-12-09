33590822d258da351937567be784bf27382fd962
nicoledodge <nicoledodge5@gmail.com>
2021-12-08 22:44:05 -0600

    fixing backend server // moving when static assets are served

--- server/server.js
+++ server/server.js
@@ -2,13 +2,21 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const routes = require('./routes');
-
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
+
 const app = express();
+
+// Serve up static assets
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -18,18 +26,12 @@ const server = new ApolloServer({
 server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
+
 app.use(express.json());
+
 app.use(routes);
 
-// Serve up static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
+// noinspection JSUnresolvedFunction
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
